117d90db157885282b7b707bcb4cdd740ae3fe08
NoahNemhed <89108122+NoahNemhed@users.noreply.github.com>
2022-03-10 15:52:29 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,7 @@ package com.example.demo3;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,19 +12,19 @@ public class Calculator {
 
 
 
-	@RequestMapping("/add/{x},{y}")
+	@RequestMapping(method = RequestMethod.GET, path = "/add/{x},{y}")
 		public double add(@PathVariable double x, @PathVariable double y) {
 			return x + y;
 			
 		}
 		
-	@RequestMapping("/multiply/{x},{y}")
+	@RequestMapping(method = RequestMethod.GET, path = "/multiply/{x},{y}")
 		public double multiply(@PathVariable double x, @PathVariable double y) {
 			return x * y;
 			
 		}
 		
-	@RequestMapping("/subtract/{x},{y}")
+	@RequestMapping(method = RequestMethod.GET, path = "/subtract/{x},{y}")
 		public double subtract(@PathVariable double x, @PathVariable double y) {
 			return x - y;
 			
